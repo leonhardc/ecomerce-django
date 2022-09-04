5fb25cbb4c20bf637f0e7f3aa9e99ed9eba70ed9
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-03 21:24:38 -0300

    adicionado app perfil na variavel INSTALLED_APPS

--- loja/settings.py
+++ loja/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'produto.apps.ProdutoConfig', # registro do app produto
     'pedido.apps.PedidoConfig', # registro do app pedido
+    'perfil.apps.PerfilConfig', # registro do app perfil
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
