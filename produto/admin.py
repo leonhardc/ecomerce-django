faf570f9e0cceb2c63a37702d403c4c3fb9c9caf
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-04 08:25:48 -0300

    Adicionado list_display

--- produto/admin.py
+++ produto/admin.py
@@ -7,6 +7,12 @@ class VariacaoInline(admin.TabularInline):
     extra = 1
 
 class ProdutoAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome',
+        'descricao_curta',
+        'preco_marketing',
+        'preco_marketing_promocional',
+    ]
     inlines = [
         VariacaoInline
     ]
