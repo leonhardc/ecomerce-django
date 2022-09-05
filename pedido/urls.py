859c67433f819df6fbfa3a20dfe0dfee38454114
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-05 15:27:18 -0300

    Adicionado endpoints/urls do app pedido

--- pedido/urls.py
+++ pedido/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
+
+app_name = 'pedido'
 
 urlpatterns = [
-
+    path('', views.Pagar.as_view(), nome='pagar'),
+    path('fecharpedido/', views.FecharPedido.as_view(), nome='fecharpedido'),
+    path('detalhe/', views.Detalhe.as_view(), nome='detalhe'),
 ]
