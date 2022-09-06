33aa8dcd21f38767312081f381e6c43866d274e9
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-06 15:26:57 -0300

    Corrigido referencia ao atributo 'name'

--- pedido/urls.py
+++ pedido/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'pedido'
 
 urlpatterns = [
-    path('', views.Pagar.as_view(), nome='pagar'),
-    path('fecharpedido/', views.FecharPedido.as_view(), nome='fecharpedido'),
-    path('detalhe/', views.Detalhe.as_view(), nome='detalhe'),
+    path('', views.Pagar.as_view(), name='pagar'),
+    path('fecharpedido/', views.FecharPedido.as_view(), name='fecharpedido'),
+    path('detalhe/', views.Detalhe.as_view(), name='detalhe'),
 ]
