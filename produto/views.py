062df88bc93113e13249cda938c227fe69ee5792
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-06 15:25:57 -0300

    Adicionada implementação da classe DetalheProduto

--- produto/views.py
+++ produto/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
-
+from .models import Produto
 
 
 class ListaProdutos(ListView):
-    pass
+    model = Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
 
-class DetalheProduto(View):
-    pass
+class DetalheProduto(DetailView):
+    model = Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 class AdicionarAoCarrinho(View):
     pass
