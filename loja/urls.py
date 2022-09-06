94122a4d0b0dfdcf6516c97193211dc4032568a4
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-06 15:17:38 -0300

    Correção de algumas configurações do projeto

--- loja/urls.py
+++ loja/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('pedido/', include('pedido.urls')),
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
