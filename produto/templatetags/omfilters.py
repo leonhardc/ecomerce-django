c2bd53d6b20521e597888339fe1ff3935c9b5b5c
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-09 16:16:01 -0300

    Implementação do filtro cart_total_qtd() - para calcular a quantidade total de produtos do carrinho do cliente

--- produto/templatetags/omfilters.py
+++ produto/templatetags/omfilters.py
@@ -1,7 +1,13 @@
 from django.template import Library
+from utils import utils
 
 register = Library()
 
 @register.filter
 def formata_preco(val):
     return f'R$ {val:.2f}'.replace('.', ',')
+
+@register.filter
+def cart_total_qtd(carrinho):
+    return utils.cart_total_qtd(carrinho)
+
