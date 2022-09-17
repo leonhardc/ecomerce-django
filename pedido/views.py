7790e792d05fb9113f5281fe17b44d204489d247
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-17 10:00:33 -0300

    primeira view de pedido

--- pedido/views.py
+++ pedido/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views import View
+from django.contrib import messages
 
 
 class Pagar(View):
-    pass
+    template_name = 'pedido/pagar.html'
+
+    def get(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                'Você precisa fazer login'
+            )
+            redirect('perfil:criar')
+
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'carrinho vazio'
+            )
+            redirect('produto:lista')
+
+
+        contexto = {}
+        return render(self.request, self.template_name, contexto)
+
 
 class SalvarPedido(View):
     pass
