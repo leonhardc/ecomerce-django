e6cd7fa3f94053306d101df4807e8ef3ace13b71
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-19 19:51:09 -0300

    Adicionada descrição sobre o app produto

--- produto/urls.py
+++ produto/urls.py
@@ -1,3 +1,32 @@
+"""
+    App produto
+
+    Este app é o app que guarda as funcionalidades voltadas para manipulação de produtos
+    no nosso e-comerce. Aqui é onde se detalha, se adiciona ao carrinho de compras etc.
+
+    As funcionalidades desse app são listadas abaixo:
+
+    1. Na url '', é a url principal da nossa aplicação, é nela que veremos todos os produtos
+    presentes na nossa base de dados. Os produtos são listados em forma de carts simples, com foto
+    nome e uma pequena descrição.
+
+    2. Na url '<slug>', expomos os detalhes a cerca do produto escolhido na página principal,
+    seu nome e sua descrição, se há variações e se o preço de cada variação.
+
+    3. Na url 'adicionaraocarrinho/', é possivel adicionar o produto selecionado/detalhado ao carrinho
+    de compras. para que possamos depois manipular nosso pedido.
+
+    4. Na url 'removerdocarrinho/', é possivel removermos algum produto do nosso carrinho antes de termi-
+    nármos o pedido.
+
+    5. Na url 'carrinho/', é possível vermos como se encontra atualmente nosso carrinho de compras.
+
+    6. Na url 'resumodacompra/', podemos finalmente ver o resumo da nossa compra, como preço final, já
+    com adicionais de frete, descontos de cupom, dentre outros.
+
+"""
+
+
 from django.urls import path
 from . import views
 
