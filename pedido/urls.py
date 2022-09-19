5d3e036abe7fa15ec55110d4efe07ba137b71fed
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-19 19:20:37 -0300

    Adicionado descrição do app pedido

--- pedido/urls.py
+++ pedido/urls.py
@@ -1,20 +1,19 @@
 """
     App pedido.
 
-        Neste app se concentra em características básicas de um pedido em
-    um processo de compra, como por exemplo, pagar o pedido, salvar o
-    pedido para pagar depois e claro, detalhar o pedido, para o cliente
-    poder revisar e ter a certeza que os produtos no carrinho são mesmo
-    o que ele quer.
+        Neste app se concentra em características básicas de um pedido em um
+    processo de compra, como por exemplo, pagar o pedido, salvar o pedido para
+    pagar depois e claro, detalhar o pedido, para o cliente poder revisar e ter
+    a certeza que os produtos no carrinho são mesmo o que ele quer.
 
-        Este app tem inicialmente, as três funcionalidades descritas logo acima:
+        Este app tem inicialmente as três funcionalidades descritas logo acima:
 
     1. Na url '', é possível escolher o método de pagamento do que está no carrinho.
     2. Na url 'salvarpedido/', é possível salvar o pedido na base de dados sem perder
     o que está no carrinho.
     3. Na url 'detalhe/', é possível detalhar o que é cada produto/variação e se ter
     noção, mesmo que de maneira redundante, do que se pediu.
-    
+
 """
 
 
