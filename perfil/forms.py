84953339a02c9c571b8a75eee9c8f56fa9f228f2
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-23 16:36:10 -0300

    Adicionado implementação parcial dos formulários de atualização de cadastro

--- perfil/forms.py
+++ perfil/forms.py
@@ -11,28 +11,29 @@ class PerfilForm(forms.ModelForm):
         fields = '__all__'
         # exclude: Campos que serão excluídos do formulário que iremos criar, nesse caso,
         # somente o campo 'username'
-        exclude = ('username',)
+        exclude = ('usuario',)
 
 class UserForm(forms.ModelForm):
     # O atributo abaixo faz com que não exibamos no formulário, se o usuário estiver logado
     # sua senha cadastrada no sistema, mas sim, um campo de senha padrão. As configurações
     # também incluem que, para a renovação do cadastro, o campo de senha não seja
-    password = forms.CharField(
-        required=False,
-        widget=forms.PasswordInput(),
-        label='Senha'
-    )
+
+    # password = forms.CharField(
+    #     required=False,
+    #     widget=forms.PasswordInput(),
+    #     label='Senha'
+    # )
+
     # Campo para confirmação da senha
-    password_confirm = forms.CharField(
-        required=False,
-        widget=forms.PasswordInput(),
-        label='Confirmar Senha'
-    )
+    # password_confirm = forms.CharField(
+    #     required=False,
+    #     widget=forms.PasswordInput(),
+    #     label='Confirmar Senha'
+    # )
 
     def __init__(self, usuario=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # Se o usuário estiver logado, saberemos que usuário envio este formulário
+        # Se o usuário estiver logado, saberemos que usuário enviou este formulário
         self.usuario = usuario
 
     class Meta:
@@ -41,8 +42,6 @@ class UserForm(forms.ModelForm):
             'username',
             'first_name',
             'last_name',
-            'password',
-            'password_confirm',
             'email',
         )
 
@@ -50,6 +49,8 @@ class UserForm(forms.ModelForm):
         data = self.data
         cleaned = self.cleaned_data
         validation_error_messages = {}
+
+        print(data)
 
         user_data = cleaned.get('username')                     # Usuário da aplicação
         password_data = cleaned.get('password')                 # Senha da aplicação
@@ -67,50 +68,52 @@ class UserForm(forms.ModelForm):
         error_msg_required_field = 'Este campo é obrigatório.'
 
 
-        # Usuário logado | Atualização
-        if self.usuario:
-            # Validação do campo 'username'
-            if user_data != user_db:
-                if user_db:
-                    validation_error_messages['username'] = error_msg_user_exists
-
-            # Validação do campo 'email'
-            if email_data != email_db:
-                if email_db:
-                    validation_error_messages['email'] = error_msg_email_exists
-
-            # Validação do campo 'password'
-            if password_data:
-
-                # Validação de conformidade
-                if password_data != password_confirm_data:
-                    validation_error_messages['password_confirm'] = error_msg_password_not_match
-
-                # Validação de tamanho
-                if len(password_data) < 6:
-                    validation_error_messages['password'] = error_msg_password_short
-
-        # Usuário não logado | Cadastro
-        else:
-            # Validação do campo 'username'
-            if user_db:
-                validation_error_messages['username'] = error_msg_user_exists
-            # Validação do campo 'email'
-            if email_db:
-                validation_error_messages['email'] = error_msg_email_exists
-            # Validação do campo 'password'
-            # Validação de conformidade
-            if not password_data:
-                validation_error_messages['password'] = error_msg_required_field
-            # Validação de não existência do campo
-            if not password_confirm_data:
-                validation_error_messages['password_confirm'] = error_msg_required_field
-
-            if password_data != password_confirm_data:
-                validation_error_messages['password_confirm'] = error_msg_password_not_match
-            # Validação de tamanho
-            if len(password_data) < 6:
-                validation_error_messages['password'] = error_msg_password_short
+        # # Usuário logado | Atualização
+        # if self.usuario:
+        #     # Validação do campo 'username'
+        #     if user_data != user_db:
+        #         if user_db:
+        #             # validation_error_messages['username'] = error_msg_user_exists
+        #             pass
+        #
+        #     # Validação do campo 'email'
+        #     if email_data != email_db:
+        #         if email_db:
+        #             # validation_error_messages['email'] = error_msg_email_exists
+        #             pass
+        #
+        #     # Validação do campo 'password'
+        #     if password_data:
+        #
+        #         # Validação de conformidade
+        #         if password_data != password_confirm_data:
+        #             validation_error_messages['password_confirm'] = error_msg_password_not_match
+        #
+        #         # Validação de tamanho
+        #         if len(password_data) < 6:
+        #             validation_error_messages['password'] = error_msg_password_short
+        #
+        # # Usuário não logado | Cadastro
+        # else:
+        #     # Validação do campo 'username'
+        #     if user_db:
+        #         validation_error_messages['username'] = error_msg_user_exists
+        #     # Validação do campo 'email'
+        #     if email_db:
+        #         validation_error_messages['email'] = error_msg_email_exists
+        #     # Validação do campo 'password'
+        #     # Validação de conformidade
+        #     if not password_data:
+        #         validation_error_messages['password'] = error_msg_required_field
+        #     # Validação de não existência do campo
+        #     if not password_confirm_data:
+        #         validation_error_messages['password_confirm'] = error_msg_required_field
+        #
+        #     if password_data != password_confirm_data:
+        #         validation_error_messages['password_confirm'] = error_msg_password_not_match
+        #     # Validação de tamanho
+        #     if len(password_data) < 6:
+        #         validation_error_messages['password'] = error_msg_password_short
 
         # Se existirem erros de validação na hora do cadastro de usuário
         if validation_error_messages:
