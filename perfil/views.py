a25ca6525ae9919acbdb6a0478b40c9dcf321939
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-09-23 16:43:11 -0300

    Adicionado corpo das views de criar usuario e atualizar cadastro de usuario

--- perfil/views.py
+++ perfil/views.py
@@ -8,7 +8,7 @@ from django.views.generic.list import ListView
 from . import models, forms
 import copy
 from django.http import HttpResponse
-
+from pprint import pprint
 
 class BasePerfil(View):
     # template_name = 'perfil/criar.html'
@@ -47,7 +47,7 @@ class BasePerfil(View):
         self.perfilform = self.contexto['perfilform']
 
         if self.request.user.is_authenticated:
-            self.template_name = 'perfil/atualizar.html '
+            self.template_name = 'perfil/criar.html'
 
         self.renderizar = render(
             self.request,
@@ -60,16 +60,22 @@ class BasePerfil(View):
 
 class Criar(BasePerfil):
     def post(self, *args, **kwargs):
-        # Se um dos dois formuçários é inválido
-        if not self.userform.is_valid() or not self.perfilform.is_valid():
+
+        # Validação para Usuário
+        if not self.userform.is_valid():
+            messages.error(
+                self.request,
+                'Formulário Invalido'
+            )
             return self.renderizar
+        else:
+            pass
 
         username = self.userform.cleaned_data.get('username')
         password = self.userform.cleaned_data.get('password')
         email = self.userform.cleaned_data.get('email')
         first_name = self.userform.cleaned_data.get('first_name')
         last_name = self.userform.cleaned_data.get('last_name')
-
 
         # Usuário Logado | Atualizar
         if self.request.user.is_authenticated:
@@ -85,38 +91,20 @@ class Criar(BasePerfil):
             usuario.email = email
             usuario.first_name = first_name
             usuario.last_name = last_name
+            usuario.save()
 
+            # Validação para perfil
             if not self.perfil:
+                pprint(self.perfilform.cleaned_data)
                 perfil = models.Perfil(**self.perfilform.cleaned_data)
+                perfil.usuario=usuario
                 perfil.save()
 
             else:
                 perfil = self.perfilform.save(commit=False)
                 perfil.usuario = usuario
                 perfil.save()
-        # Usuário não logado (Novo usuário)
-        else:
-            # não salvar o usuário ainda na base de dados
-            usuario = self.userform.save(commit=False)
-            usuario.set_password(password)
-            usuario.save()
 
-            perfil = self.perfilform.save(commit=False)
-            perfil.usuario = usuario
-            perfil.save()
-
-        if password:
-            autentica = authenticate(
-                self.request,
-                username=usuario,
-                password=password
-            )
-
-            if autentica:
-                login(
-                    self.request,
-                    user=usuario
-                )
 
         self.request.session['carrinho'] = self.carrinho
         self.request.session.save()
@@ -126,35 +114,87 @@ class Criar(BasePerfil):
             'Seu cadastro foi criado ou atualizado com sucesso.'
         )
 
-        messages.success(
-            self.request,
-            'Você fez login e pode concluir sua compra.'
-        )
+        if self.request.session['carrinho']:
+            messages.success(
+                self.request,
+                'Você fez login e pode concluir sua compra.'
+            )
 
         return redirect('perfil:criar')
+
+class CriarUsuario(View):
+    def post(self, *args, **kwargs):
+        novo_usuario = self.request.POST.get('newuser')
+        email = self.request.POST.get('email')
+        senha = self.request.POST.get('pswd')
+
+        pprint(self.request.POST) #TODO: Apagar esta linha
+
+        # Verificar se o usuário já está cadastrado
+        if User.objects.filter(username=novo_usuario):
+            messages.error(
+                self.request,
+                'Usuário já existe'
+            )
+            return redirect('perfil:criar')
+        # Verificar se o email já está cadastrado
+        if User.objects.filter(email=email):
+            messages.error(
+                self.request,
+                'Email já existe'
+            )
+            return redirect('perfil:criar')
+
+        # Criando novo usuário django
+        user = User.objects.create_user(username=novo_usuario,
+                                        email=email,
+                                        password=senha)
+        # Fazer login
+        autentica = authenticate(
+            self.request,
+            username=novo_usuario,
+            password=senha
+        )
+        if autentica:
+            login(
+                self.request,
+                autentica
+            )
+        return render(self.request, 'perfil/cadastro_ou_pagprincipal.html')
+
+class ValidarUsuario(BasePerfil):
+    def post(self, *args, **kwargs):
+        if not self.userform.is_valid(): # Se o formulário é válido
+
+            username = self.userform.cleaned_data.get('username')
+            password = self.userform.cleaned_data.get('password')
+            email = self.userform.cleaned_data.get('email')
+            first_name = self.userform.cleaned_data.get('first_name')
+            last_name = self.userform.cleaned_data.get('last_name')
+
+            if self.request.user.is_authenticated:
+                usuario = get_object_or_404(
+                    User,
+                    username=self.request.user.username
+                )
+
+                usuario.username = username
+
+                usuario.email = email
+                usuario.first_name = first_name
+                usuario.last_name = last_name
+                usuario.save()
+
+        else: # Se o formulário de usuário não for válido
+            messages.error(
+                self.request,
+                'Formulario Invalido'
+            )
+            return self.renderizar
 
 class Atualizar(View):
     def get(self, *args, **kwargs):
         return render(self.request, template_name='perfil/atualizar.html')
-
-# def login(request):
-#
-#     if request.method != 'POST':
-#         # verifica se o formulário esta vazio
-#         return redirect('perfil:criar')
-#     usuario = request.POST.get('usuario')
-#     senha = request.POST.get('senha')
-#     # autenticar usuario
-#     user = auth.authenticate(request, username=usuario, password=senha)
-#     # se o usuário não autenticar, a função auth.authenticate() irá retornar None
-#     if not user:
-#         messages.error(request, 'Usuário ou Senha inválidos.')
-#         return redirect('perfil:criar')
-#     else:
-#         auth.login(request, user) #faz login
-#         messages.success(request, 'Voce fez login com sucesso.')
-#         return redirect('produto:lista')
-
 
 class Login(View):
     def post(self, *args, **kwargs):
@@ -184,7 +224,6 @@ class Login(View):
             )
             return redirect('produto:lista')
 
-
 class Logout(View):
     def get(self, *args, **kwargs):
         carrinho = copy.deepcopy(self.request.session.get('carrinho'))
@@ -194,7 +233,6 @@ class Logout(View):
         logout(self.request)
         return redirect('produto:lista')
 
-
 class DetalhePerfil(ListView):
     """
         Detalha os dados do perfil do usuário
@@ -203,5 +241,25 @@ class DetalhePerfil(ListView):
     model = models.Perfil
     template_name = 'perfil/detalhe.html'
     context_object_name = 'perfil'
+
+# def login(request):
+#
+#     if request.method != 'POST':
+#         # verifica se o formulário esta vazio
+#         return redirect('perfil:criar')
+#     usuario = request.POST.get('usuario')
+#     senha = request.POST.get('senha')
+#     # autenticar usuario
+#     user = auth.authenticate(request, username=usuario, password=senha)
+#     # se o usuário não autenticar, a função auth.authenticate() irá retornar None
+#     if not user:
+#         messages.error(request, 'Usuário ou Senha inválidos.')
+#         return redirect('perfil:criar')
+#     else:
+#         auth.login(request, user) #faz login
+#         messages.success(request, 'Voce fez login com sucesso.')
+#         return redirect('produto:lista')
+
+
 
 
