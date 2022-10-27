491cb274b84f7a056615ab6a3c4b1009f1ad67f9
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-10-27 19:01:18 -0300

    Adicionado descrição do model do app

--- comentarios/models.py
+++ comentarios/models.py
@@ -1,3 +1,14 @@
+"""
+    Lembrar que os comentários tem que ser associados ao produto a qual se 
+    está avaliando e ao usuário que está avaliando determinado produto.
+
+    Obs: O comentário/avaliação só deve ser realizada pelos usuários que
+    tiverem comprado o produto e recebido.
+
+    Obs: Fazer um sistema de comentários, e avaliações com estrela
+
+"""
+
 from django.db import models
 
 # Create your models here.
