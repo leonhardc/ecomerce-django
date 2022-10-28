1d1f4128a8a7f032154b52f8f738048deba77a6c
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-10-28 13:33:11 -0300

    Adicionado novo campo no banco de dados para guardar foto de perfil e método para resize da imagem escolhida

--- perfil/models.py
+++ perfil/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ValidationError
 from utils.utils import valida_cpf
+from PIL import Image
+import os
 import re
 
 
@@ -9,9 +11,15 @@ class Perfil(models.Model):
 
     usuario = models.OneToOneField(User, on_delete=models.CASCADE) # se o usuário for apagado, o perfil 
                                                                    # também será apagado
+    foto_perfil = models.ImageField(
+        upload_to='perfil_photo/%Y/%m/',
+        blank= True,
+        null= True)
+    
     idade = models.PositiveIntegerField()
     data_nascimento = models.DateField()
     cpf = models.CharField(max_length=11)
+    
     endereco = models.CharField(max_length=50)
     numero = models.CharField(max_length=5)
     complemento = models.CharField(max_length=30)
@@ -51,6 +59,25 @@ class Perfil(models.Model):
             ('TO', 'Tocantins'),
         )
     )
+
+    @staticmethod
+    def resize_image(img, max_width=800):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name) # caminho completo da imagem
+        image_pil = Image.open(img_full_path) # abrir imagem com pil
+        original_width, original_height = image_pil.size # altura e largura original da imagem
+
+        if original_width <= max_width:
+            image_pil.close() # fechando a imagem aberta anteriormente
+            return # se a largura original for menor que a nova largura, não precisa
+                   # fazer o redimensionamento
+
+        new_height = round((max_width * original_height) / original_width) # descobrir nova altura
+        new_image = image_pil.resize((max_width, new_height), Image.LANCZOS) # redimensiona imagem
+        new_image.save(         # salva a nova imagem por cima da antiga
+            img_full_path,      # onde se deseja salvar a imagem
+            optimize=True,
+            quality=50          # reduz a qualidade para 50%
+        )
 
     def __str__(self):
         return f'{self.usuario.first_name}'
