321adf7ef84d4a194bca6c40f9ff8a0b0c07b3ea
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-11-19 10:31:04 -0300

    Adicionado model do app comentário

--- comentarios/models.py
+++ comentarios/models.py
@@ -1,14 +1,17 @@
-"""
-    Lembrar que os comentários tem que ser associados ao produto a qual se 
-    está avaliando e ao usuário que está avaliando determinado produto.
-
-    Obs: O comentário/avaliação só deve ser realizada pelos usuários que
-    tiverem comprado o produto e recebido.
-
-    Obs: Fazer um sistema de comentários, e avaliações com estrela
-
-"""
-
 from django.db import models
+from produto.models import Produto
+from django.contrib.auth.models import User  # para usuario_comentario
+from django.utils import timezone  # para data_comentario
 
-# Create your models here.
+
+class Comentario(models.Model):
+    nome_comentario = models.CharField(max_length=150, verbose_name='Nome')
+    email_comentario = models.EmailField(verbose_name='Email')
+    comentario = models.TextField(verbose_name='Comentario')
+    produto_comentario = models.ForeignKey(Produto, on_delete=models.CASCADE, verbose_name='Produto ID')
+    usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Autor ID', blank=True, null=True)
+    data_comentario = models.DateTimeField(default=timezone.now, verbose_name='Publicado em')
+    publicado_comentario = models.BooleanField(default=False, verbose_name='Publicar')
+
+
+
