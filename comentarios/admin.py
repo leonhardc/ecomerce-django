c082c78000ba8603dd75b42125adbab1de2e3e86
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-11-19 10:31:49 -0300

    App comentário registrado na zona administrativa

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Comentario
 
-# Register your models here.
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome_comentario',
+        'email_comentario',
+        'produto_comentario',
+        'produto_comentario',
+        'usuario_comentario',
+        'data_comentario',
+        'publicado_comentario',
+    ]
+
+admin.site.register(Comentario, ComentarioAdmin)
+
+
