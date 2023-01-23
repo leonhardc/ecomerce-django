831e11fe0503ca7d74031811ff7418cd41f34bc9
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-01-23 13:06:39 -0300

    Mudança do model dos comentarios

--- comentarios/models.py
+++ comentarios/models.py
@@ -5,13 +5,11 @@ from django.utils import timezone  # para data_comentario
 
 
 class Comentario(models.Model):
-    nome_comentario = models.CharField(max_length=150, verbose_name='Nome')
-    email_comentario = models.EmailField(verbose_name='Email')
     comentario = models.TextField(verbose_name='Comentario')
     produto_comentario = models.ForeignKey(Produto, on_delete=models.CASCADE, verbose_name='Produto ID')
     usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Autor ID', blank=True, null=True)
     data_comentario = models.DateTimeField(default=timezone.now, verbose_name='Publicado em')
-    publicado_comentario = models.BooleanField(default=False, verbose_name='Publicar')
+    publicado_comentario = models.BooleanField(default=True, verbose_name='Publicar')
 
 
 
